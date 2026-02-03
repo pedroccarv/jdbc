996ad8050e0c7f5ab5c89393a4f5ff1a718cd57e
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-03 09:20:13 -0300

    add: recuperando dados

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
